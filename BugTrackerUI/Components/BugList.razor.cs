e412d0a8286b0e321413495174b2ad3e085b7d23
Mina Shokralla <mina.shokralla@yahoo.com>
2021-11-14 14:53:46 +0100

    Display the Bugs using a Component

---
+++ BugTrackerUI/Components/BugList.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BugTrackerUI.Components
+{
+  public partial class BugList
+  {
+
+    [Inject]
+    public IBugService BugService { get; set; }
+    public List<Bug> Bugs { get; set; }
+
+    protected override void OnInitialized()
+    {
+      base.OnInitialized();
+      Bugs = BugService.GetBugs().OrderBy(x => x.Priority).ToList();
+
+
+    }
+  }
+}
